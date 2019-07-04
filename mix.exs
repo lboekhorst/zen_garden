88a8188d667c33272b424a796cbd195139b3da8b
Laurens Boekhorst <laurens.boekhorst@gmail.com>
2019-07-04 17:30:23 +0200

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -28,9 +28,7 @@ defmodule ZenGarden.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 
